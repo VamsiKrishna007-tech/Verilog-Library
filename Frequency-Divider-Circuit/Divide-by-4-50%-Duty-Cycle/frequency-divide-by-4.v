c946f04bb01d76d5eb878cc99a4f21267fa8882f
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2026-06-25 17:23:24 +0530

    Update frequency-divide-by-4.v

--- Frequency-Divider-Circuit/Divide-by-4-50%-Duty-Cycle/frequency-divide-by-4.v
+++ Frequency-Divider-Circuit/Divide-by-4-50%-Duty-Cycle/frequency-divide-by-4.v
@@ -1,1 +1,14 @@
-mm
+module divide_by_4(input input_clk, input rst, output reg output_clk);
+reg [1:0] count;
+always @(posedge input_clk or negedge rst) begin
+    if (!rst)
+      count <= 0;
+    else if (count == 3) 
+      count <= 0;
+    else 
+      count <= count + 1;
+end
+always @(*) begin
+output_clk = count[1];
+end
+endmodule
